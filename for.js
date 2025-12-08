b9852a2c70ac0cf9babc6a682afdfda15725177e
dishant-0202 <dishantrudani592@gmail.com>
2025-12-08 16:28:27 +0530

    for loop

---
+++ for.js
@@ -0,0 +1,13 @@
+for(i=0;i<=5;i++)
+{
+    console.log("number is :" + i);
+}
+
+
+const cars=["bmw" , "volvo" , "tata"  , "honda" , "hundai" , "nishan" ]
+let len=cars.length
+
+for (let i=0 ; i< len ; i++)
+{
+    console.log(cars[i]);
+}
